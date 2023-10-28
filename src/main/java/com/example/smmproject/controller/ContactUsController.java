bbb988fa171fc05dfba828380c522f2f65761ef5
bayram-huseynli <bayram.huseynli10@gmail.com>
2023-10-28 12:01:34 +0400

    Controllers are ready!

--- src/main/java/com/example/smmproject/controller/ContactUsController.java
+++ src/main/java/com/example/smmproject/controller/ContactUsController.java
@@ -1,8 +1,48 @@
 package com.example.smmproject.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.smmproject.dto.Request.ContactUsRequest;
+import com.example.smmproject.dto.Response.ContactUsResponse;
+import com.example.smmproject.service.ContactUsService;
+import org.springframework.boot.autoconfigure.session.RedisSessionProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/contactUs")
 public class ContactUsController {
+
+    private final ContactUsService contactUsService;
+
+    public ContactUsController(ContactUsService contactUsService) {
+        this.contactUsService = contactUsService;
+    }
+
+    @GetMapping("getAll")
+    public ResponseEntity<List<ContactUsResponse>> findAll(){
+        return new ResponseEntity<>(contactUsService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public void create (@RequestBody ContactUsRequest contactUsRequest){
+        contactUsService.create(contactUsRequest);
+    }
+
+    @GetMapping("getById/{id}")
+    public ContactUsResponse getById(@PathVariable Long id){
+        return contactUsService.getById(id);
+    }
+
+    @PutMapping("update/{id}")
+    public void update(@PathVariable Long id,@RequestBody ContactUsRequest contactUsRequest){
+        contactUsService.update(id,contactUsRequest);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public void delete(@PathVariable Long id){
+        contactUsService.delete(id);
+    }
 
 }
--- src/main/java/com/example/smmproject/controller/ManagerController.java
+++ src/main/java/com/example/smmproject/controller/ManagerController.java
@@ -1,7 +1,47 @@
 package com.example.smmproject.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.smmproject.dto.Request.ManagerRequest;
+import com.example.smmproject.dto.Response.ManagerResponse;
+import com.example.smmproject.service.ManagerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("api/manager")
 public class ManagerController {
+
+    private final ManagerService managerService;
+
+    public ManagerController(ManagerService managerService) {
+        this.managerService = managerService;
+    }
+
+    @GetMapping("getAll")
+    public ResponseEntity<List<ManagerResponse>> findAll(){
+        return new ResponseEntity<>(managerService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public void create (@RequestBody ManagerRequest managerRequest){
+        managerService.create(managerRequest);
+    }
+
+    @GetMapping("getById/{id}")
+    public ManagerResponse getById(@PathVariable Long id){
+        return managerService.getById(id);
+    }
+
+    @PutMapping("update/{id}")
+    public void update(@PathVariable Long id,@RequestBody ManagerRequest managerRequest){
+        managerService.update(id,managerRequest);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public void delete(@PathVariable Long id){
+        managerService.delete(id);
+    }
+
 }
